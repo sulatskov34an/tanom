2387698b56a66461a23eb97e75edeb6f76aab941
Sulatskov Sergey <sulatskov.s@r-mades.com>
2020-11-01 21:48:48 +0400

    добавлены зависимости compose

--- app/src/main/java/ru/tanom/ui/ad_details/AdDetailsFragment.kt
+++ app/src/main/java/ru/tanom/ui/ad_details/AdDetailsFragment.kt
@@ -2,20 +2,17 @@ package ru.tanom.ui.ad_details
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import androidx.navigation.fragment.findNavController
-import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.fragment_ad_details.*
-import kotlinx.android.synthetic.main.fragment_ad_details.view.*
+import androidx.compose.Composable
+import androidx.ui.core.Text
+import androidx.ui.core.setContent
+import androidx.ui.layout.FlexColumn
+import androidx.ui.material.MaterialTheme
+import androidx.ui.material.TopAppBar
 import ru.tanom.R
 import ru.tanom.base.view.BaseFragment
-import ru.tanom.base.viewmodel.Status
-import ru.tanom.common.*
-import ru.tanom.model.network.dto.Ad
-import ru.tanom.ui.MainActivity
 
 class AdDetailsFragment : BaseFragment() {
 
@@ -27,50 +24,32 @@ class AdDetailsFragment : BaseFragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_ad_details, container, false)
+
+        (root as ViewGroup).setContent {
+            Hello("Jetpack Compose")
+        }
+
         return root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        (activity as? MainActivity)?.hideBottomNavigation()
-        view.content.gone()
-        val id = arguments?.getInt(AppConst.AD_ID_KEY, 0)
-        adDetailsViewModel = ViewModelProviders.of(this).get(AdDetailsViewModel::class.java)
-        adDetailsViewModel.ad.observe(this.viewLifecycleOwner, Observer {
-            when (it.status) {
-                Status.LOADING -> onProgress()
-                Status.SUCCESS -> onSuccess(it.data?.result)
-                Status.ERROR -> onError()
+    @Composable
+    fun Hello(name: String) = MaterialTheme {
+        FlexColumn {
+            inflexible {
+                // Item height will be equal content height
+                TopAppBar<MenuItem>( // App Bar with title
+                    title = { Text("Jetpack Compose Sample") }
+                )
             }
-        })
-        adDetailsViewModel.getAd(id)
+            expanded(1F) {
+                // occupy whole empty space in the Column
+                // Center content
+                Text("Hello $name!") // Text label
+            }
+        }
     }
 
-
     override fun <T> onSuccess(content: T?) {
-        view?.content?.visible()
-        hideProgress()
-        val ad = (content as? Ad)
-        view?.description?.text = ad?.description.toString()
-        context?.let {
-            view?.image?.apply {
-                val path = "saffdasfd"
-                Picasso.get()
-                    .load(path)
-                    .error(R.drawable.ic_error)
-                    .placeholder(getProgressBar(context = it))
-                    .into(image)
-            }
-        }
-        view?.title?.text =
-            "${ad?.carFactory} ${ad?.carModel}, ${ad?.productionYear}, ${ad?.mileage}"
-        view?.inspectionPlace?.text = ad?.inspectionPlace.toString()
-        view?.price?.text = "${ad?.price} ₽"
-        view?.context?.let {
-            view?.creation_date?.text =
-                TimeUtils(it).getDateWithInTime(it, ad?.creationDate ?: 0)
-        }
-
     }
 
     override fun showPlaceholder() {
@@ -80,7 +59,6 @@ class AdDetailsFragment : BaseFragment() {
     }
 
     override fun setToolbar() {
-        text_toolbar.setText(R.string.search)
-        toolbar.setOnClickListener{ findNavController().navigate(R.id.action_to_ads_search) }
     }
+
 }
