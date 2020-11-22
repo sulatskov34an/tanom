5c2e4c3d3283b14db68db9d90b3486d204737fce
Sulatskov Sergey <sulatskov.s@r-mades.com>
2020-11-23 00:04:54 +0400

    feature(search): верска

--- app/src/main/java/ru/tanom/ui/search/ShimmerAdsAdapter.kt
+++ app/src/main/java/ru/tanom/ui/search/ShimmerAdsAdapter.kt
@@ -6,12 +6,9 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import ru.tanom.R
 
-class ShimmerAdsAdapter() :
-
-    RecyclerView.Adapter<ShimmerAdsAdapter.ShimmerViewHolder>() {
+class ShimmerAdsAdapter : RecyclerView.Adapter<ShimmerAdsAdapter.ShimmerViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShimmerViewHolder {
-
         return ShimmerViewHolder(
             LayoutInflater.from(parent.context).inflate(
                 R.layout.item_shimmer_search,
